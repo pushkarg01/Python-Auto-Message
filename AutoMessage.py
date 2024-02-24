2e29add6b8323ea3f2bab81cb08280ff89523b11
pushkargupta290 <125808807+pushkargupta290@users.noreply.github.com>
2024-02-24 14:00:30 +0530

    Update Comments

--- AutoMessage.py
+++ AutoMessage.py
@@ -1,8 +1,10 @@
 import pyautogui
 import time
 time.sleep(5)
+#Enter how many times you want to send a message
 a=10
 while a>=1:
+    #Enter your message
     pyautogui.typewrite("Hello Friend")
     time.sleep(0)
     pyautogui.press("enter")
